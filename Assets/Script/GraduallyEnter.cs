86ba4a51b6df8da644ad407020567becccbd5025
home-pc <84121599@qq.com>
2020-06-25 01:04:31 +0800

    布置了简单场景;添加了进入渐变UI

---
+++ Assets/Script/GraduallyEnter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class GraduallyEnter : MonoBehaviour
+{
+    Image graduallyMask;
+    public float graduallyTime;
+
+    void Start()
+    {
+        graduallyMask = GetComponent<Image>();
+        if (graduallyMask != null)
+        {
+            Color endColor = graduallyMask.color;
+            endColor.a = 0;
+            graduallyMask.DOColor(endColor, graduallyTime);
+        }
+
+    }
+    
+    void Update()
+    {
+        
+    }
+}
