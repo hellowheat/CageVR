b9938bde95b9f8ccef7f318eac388bf5ed889fcf
home-pc <84121599@qq.com>
2020-07-05 12:22:15 +0800

    完成了罐子破碎效果；为随机场景对象添加了说明；优化了虚无场景。

---
+++ Assets/Script/AutoDestory.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDestory : MonoBehaviour
+{
+    public float destoryTime = 10;
+    float checkCD = 1;
+    float checkTime = 0;
+    float waitTime = 0;
+    Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        waitTime += Time.deltaTime;
+        checkTime += Time.deltaTime;
+        if (checkTime > checkCD && rb != null)
+        {
+            if (rb.velocity != Vector3.zero) waitTime = 0;
+        }
+
+        if (waitTime >= destoryTime)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
--- Assets/Script/CanBeBreak.cs
+++ Assets/Script/CanBeBreak.cs
@@ -5,9 +5,21 @@ using EzySlice;
 
 public class CanBeBreak : MonoBehaviour
 {
-    private void Update()
+    public int cullTime;//切割迭代次数
+    public Material cutMaterial;//切割的材质，没有就用原材质
+    public float centerOffsetY;//中心偏移
+    public float removeTime;//碎片移除时间
+    public float speedThreashold;//破裂速度阈值
+
+    private void OnCollisionEnter(Collision collision)
     {
-        if (Input.GetKeyDown(KeyCode.U))
+        Rigidbody myrb = gameObject.GetComponent<Rigidbody>();
+        Rigidbody crb = collision.transform.GetComponent<Rigidbody>();
+        if (myrb != null) Debug.Log(myrb.velocity+",my");
+        if (crb != null) Debug.Log(crb.velocity+",collision");
+
+        if ((myrb != null && myrb.velocity.magnitude >= speedThreashold) 
+            || (crb != null && crb.velocity.magnitude >= speedThreashold))
         {
             beBreak();
         }
@@ -15,28 +27,35 @@ public class CanBeBreak : MonoBehaviour
 
     void beBreak()
     {
-        sliceObj(gameObject, 5);
+        sliceObj(gameObject, cullTime, new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)));
     }
 
-    void sliceObj(GameObject gb,int time)
+    void sliceObj(GameObject gb,int time,Vector3 dir)
     {
         if (time <= 0) return;
-        SlicedHull hull = gb.Slice(gb.transform.position,new Vector3(Random.Range(-1f,1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)));
+        SlicedHull hull = gb.Slice(transform.position+new Vector3(0, centerOffsetY, 0), 
+            dir);
+        Vector3[] rotateType = { Vector3.up, Vector3.right, Vector3.forward };
+        Material material = cutMaterial? cutMaterial:gb.GetComponent<Renderer>().material;
         if(hull != null)
         {
             if (hull.lowerHull != null)
             {
-                GameObject lowGB = hull.CreateLowerHull();
+                GameObject lowGB = hull.CreateLowerHull(gb, material);
+                lowGB.transform.position = gb.transform.position;
                 lowGB.AddComponent<BoxCollider>();
                 lowGB.AddComponent<Rigidbody>();
-                sliceObj(lowGB, time - 1);
+                if (removeTime >= 0) lowGB.AddComponent<AutoDestory>().destoryTime = removeTime ;
+                sliceObj(lowGB, time - 1, Quaternion.AngleAxis(60, rotateType[Random.Range(0, 3)]) * dir);
             }
             if (hull.upperHull != null)
             {
-                GameObject upGB = hull.CreateLowerHull();
+                GameObject upGB = hull.CreateUpperHull(gb, material);
+                upGB.transform.position = gb.transform.position;
                 upGB.AddComponent<BoxCollider>();
                 upGB.AddComponent<Rigidbody>();
-                sliceObj(upGB, time - 1);
+                if (removeTime >= 0) upGB.AddComponent<AutoDestory>().destoryTime = removeTime;
+                sliceObj(upGB, time - 1, Quaternion.AngleAxis(60, rotateType[Random.Range(0, 3)]) * dir);
             }
 
             Destroy(gb);
