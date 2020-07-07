7e4791c895ce5681e830f9c3a6dc97b7f12fdfe9
htp <84121599@qq.com>
2020-07-07 23:11:38 +0800

    测试steamVR场景；添加交互手到场景中

--- Assets/ExtratPackage/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ Assets/ExtratPackage/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -105,6 +105,14 @@ namespace Valve.VR
             }
         }
         
+        public virtual SteamVR_Action_Boolean sayHello
+        {
+            get
+            {
+                return SteamVR_Actions.default_sayHello;
+            }
+        }
+        
         public virtual SteamVR_Action_Vibration Haptic
         {
             get
